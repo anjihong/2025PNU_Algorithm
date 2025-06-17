16ec2b3e8e1408f1b153da8096c1131ccd0618a6
hong <anjhu@pusan.ac.kr>
2025-06-17 20:38:58 +0900

    17번과제수정, 18번 과제 수정했지만 실패..

--- 18_station.cpp
+++ 18_station.cpp
@@ -4,23 +4,19 @@ using namespace std;
 
 struct circle
 {
-    int x, y, r;
+    double x, y, r;
+    int id;
     double common_circle(circle &other)
     {
-        double d = hypot(x - other.x, y - other.y);
-        if (d >= r + other.r)
-            return 0.0;
-        else if (d <= abs(r - other.r))
-            return PI * min(r, other.r) * min(r, other.r);
-        else
-        {
-            double angle1 = acos((pow(r, 2) + pow(d, 2) - pow(other.r, 2)) / (2 * r * d));
-            double angle2 = acos((pow(other.r, 2) + pow(d, 2) - pow(r, 2)) / (2 * other.r * d));
-            double area1 = angle1 * r * r;
-            double area2 = angle2 * other.r * other.r;
-            double area3 = 0.5 * sqrt((-d + r + other.r) * (d + r - other.r) * (d - r + other.r) * (d + r + other.r));
-            return area1 + area2 - area3;
-        }
+        double dx = x - other.x, dy = y - other.y;
+        double d = sqrt(dx * dx + dy * dy);
+        if (d >= r + other.r) return 0.0; // 겹치지 않음
+        if (d <= fabs(r - other.r)) return PI * min(r, other.r) * min(r, other.r); // 한 원이 다른 원을 포함함
+        double r1 = r, r2 = other.r;
+        double alpha = acos((r1 * r1 + d * d - r2 * r2) / (2 * r1 * d));
+        double beta = acos((r2 * r2 + d * d - r1 * r1) / (2 * r2 * d));
+        double seg = 0.5 * sqrt(max(0.0, (-d + r1 + r2) * (d + r1 - r2) * (d - r1 + r2) * (d + r1 + r2)));
+        return r1 * r1 * alpha + r2 * r2 * beta - seg; // 겹치는 부분의 면적
     }
 };
 
@@ -29,19 +25,15 @@ int main()
     int n;
     cin >> n;
     vector<circle> circles(n);
-    int dummy;
     for (int i = 0; i < n; ++i)
     {
-        cin >> dummy >> circles[i].x >> circles[i].y >> circles[i].r;
+        cin >> circles[i].id >> circles[i].x >> circles[i].y >> circles[i].r;
     }
 
-    sort(circles.begin(), circles.end(), [](circle &a, circle &b)
-         {
-             return a.x < b.x; // x 기준으로 정렬
-         });
+    sort(circles.begin(), circles.end(), [](circle &a, circle &b) {return a.x < b.x; });
 
     double max_area = 0.0;
-    int idx1 = -1, idx2 = -1;
+    int idx1 = INT_MAX, idx2 = INT_MAX;
 
     for (int i = 0; i < n; ++i)
     {
@@ -49,17 +41,20 @@ int main()
         {
             // x 좌표 거리만으로 겹침 여부 미리 판단
             if (circles[j].x - circles[i].x > circles[i].r + circles[j].r)
-                break;
+                continue;
 
             double area = circles[i].common_circle(circles[j]);
             double cut_area = floor(area * 1000) / 1000;
             double max_cut = floor(max_area * 1000) / 1000;
-
-            if (cut_area > max_cut || (cut_area == max_cut && make_pair(i + 1, j + 1) < make_pair(idx1, idx2)))
+            
+            int id1, id2;
+            circles[i].id < circles[j].id ? (id1 = circles[i].id, id2 = circles[j].id)
+                                           : (id1 = circles[j].id, id2 = circles[i].id);
+            if (cut_area > max_cut || (cut_area == max_cut && make_pair(id1, id2) < make_pair(idx1, idx2)))
             {
                 max_area = cut_area;
-                idx1 = i + 1;
-                idx2 = j + 1;
+                idx1 = id1;
+                idx2 = id2;
             }
         }
     }
